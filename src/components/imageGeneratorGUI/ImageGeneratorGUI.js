b40d35cc9d1d66758db47c2b1c5ea7aa61ffa858
EvanGottschalk <magnus5557@gmail.com>
2024-10-28 01:12:34 -0400

    added select model

--- src/components/imageGeneratorGUI/ImageGeneratorGUI.js
+++ src/components/imageGeneratorGUI/ImageGeneratorGUI.js
@@ -25,6 +25,12 @@ const ImageGeneratorGUI = () => {
 
   function handlePromptChange(event) {
     let prompt = event.target.value;
+    console.log("Current Prompt:", prompt);
+  }
+
+  function handleModelChange(event) {
+    let model = event.target.value;
+    console.log("Selected Model:", model);
   }
 
   return (
@@ -35,13 +41,22 @@ const ImageGeneratorGUI = () => {
           {/* <div className='imageGeneratorGUIText imageGeneratorGUITagline' data-aos="fade-right" data-aos-delay="200" style={{
             textDecoration: 'none',
             color: '#00ef63'}}>- Blockchain Onboarding Solutions -</div> */}
-          <div className='imageGeneratorGUIText' data-aos="fade-right" data-aos-delay="300" style={{
+          <div className='imageGeneratorGUITitle' data-aos="fade-right" data-aos-delay="300" style={{
             textDecoration: 'none',
             color: '#bbbbbb'}}>Enter Prompt:
           </div>
             <textarea id='imageGeneratorGUIEntry' className='imageGeneratorGUIEntry' data-aos="fade-right" data-aos-delay="300" style={{
               textDecoration: 'none',
               color: '#000000'}} placeholder="Your prompt here..." onChange={handlePromptChange} required/>
+          <div className='imageGeneratorGUITitle' data-aos="fade-right" data-aos-delay="300" style={{
+            textDecoration: 'none',
+            color: '#bbbbbb'}}>Select Model:
+          </div>
+          <select className='imageGeneratorGUI_selectList' id='selectModel' onChange={handleModelChange}>
+            <option value="black-forest-labs/FLUX.1-dev">black-forest-labs/FLUX.1-dev</option>
+            <option value="black-forest-labs/FLUX.1-dev0">black-forest-labs/FLUX.1-dev</option>
+            <option value="black-forest-labs/FLUX.1-dev">black-forest-labs/FLUX.1-dev</option>
+          </select>
           {/* <a data-aos="flip-down" data-aos-delay="0" href='https://evanon.earth/nofunlabs' target="_blank" 
           className='imageGeneratorGUIText imageGeneratorGUILink' id='nofunlabsLink' onMouseOver={mouseOver} onMouseLeave={mouseLeave} style={{
             textDecoration: 'none',
